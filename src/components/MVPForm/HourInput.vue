a72ecaa274b309cc49f3eaf79742de4f754b8680
Error471 <ivanscultural12@gmail.com>
2023-02-25 14:10:05 +0100

    Add files via upload

---
+++ src/components/MVPForm/HourInput.vue
@@ -0,0 +1,33 @@
+<template>
+  <input
+    type="text"
+    class="form-control"
+    name="hour"
+    id="hour"
+    placeholder="Type Hour"
+    :value="modelValue"
+    @keyup="maskHour($event)"
+    @input="$emit('update:modelValue', $event.target.value)"
+  />
+</template>
+<script>
+export default {
+    name: 'HourInput',
+    props: {
+      modelValue: String
+    },
+    methods: {
+      maskHour: function(event) {
+         if (event.target.value.length == 2) {
+           if (event.keyCode == 8) {
+            return
+           }
+           else {
+            event.target.value += ":";
+           }
+        }
+      }
+    }
+};
+</script>
+<style></style>
---
+++ src/components/MVPForm/MVPListSelect.vue
@@ -0,0 +1,31 @@
+<template>
+  <select
+    name="mvp-list"
+    id="mvp-list"
+    class="form-control col-md-12"
+    @change="changeMVP($event)"
+  >
+    <option value="0">-</option>
+    <option v-for="(mvp, index) in mvp_list" :value="mvp.id" :key="index">
+      {{ mvp.name }}
+    </option>
+  </select>
+</template>
+<script>
+import { mapState, mapMutations } from "vuex";
+export default {
+  name: "MVPListSelect",
+  props: {
+    modelValue: String,
+  },
+  computed: mapState(["mvp_list"]),
+  methods: {
+    ...mapMutations(["setCurrentMvpId"]),
+    changeMVP: function (event) {
+      this.setCurrentMvpId(event.target.value);
+      this.$emit("update:modelValue", event.target.value);
+    },
+  },
+};
+</script>
+<style></style>
