0ae52f0fa43a64eb0e768c304c7865f86cc718a9
Error471 <ivanscultural12@gmail.com>
2023-02-25 14:08:46 +0100

    Add files via upload

---
+++ mvp-timer-main/src/components/MVPForm/MVPMapListSelect.vue
@@ -0,0 +1,57 @@
+<template>
+  <select
+    name="mvp-map"
+    id="mvp-map"
+    class="form-control col-md-12"
+    @change="changeMVPMap($event)"
+  >
+    <option
+      v-for="(respawn, index) in map_list"
+      :value="respawn.map"
+      :id="respawn.map"
+      :key="index"
+    >
+      {{ respawn.map }} ({{ parseInt(respawn.delay / 60000) }} min)
+    </option>
+  </select>
+</template>
+<script>
+import { mapGetters } from "vuex";
+export default {
+  name: "MVPMapListSelect",
+  props: {
+    modelValue: String,
+  },
+  data() {
+    return {
+      map_list: [],
+    };
+  },
+  methods: {
+    updateMapList: function (id) {
+      if (id == 0) {
+        this.map_list = [];
+      } else {
+        let mvp = this.getMVPById(parseInt(id));
+        this.map_list = mvp.respawn;
+        this.$emit("update:modelValue", this.map_list[0].map);
+      }
+    },
+    changeMVPMap: function (event) {
+      this.$emit("update:modelValue", event.target.value);
+    },
+  },
+  computed: {
+    ...mapGetters(["getMVPById"]),
+  },
+  watch: {
+    "$store.state.current_mvp_id": {
+      deep: true,
+      handler(newValue) {
+        this.updateMapList(newValue);
+      },
+    },
+  },
+};
+</script>
+<style></style>
